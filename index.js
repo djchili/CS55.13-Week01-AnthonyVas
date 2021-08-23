a8661b1ee78167aea7cb558441cd68b2baae6152
AnthonyVas <89411083+djchili@users.noreply.github.com>
2021-08-23 18:00:52 +0000

    made a server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-Console.log(5*5);
+let myhttp = require('http');
+let myserver = myhttp.createServer(
+  function(myrequest, myresponse) {
+    console.log( myrequest.url );
+    let mytext;
+    if ( myrequest.url === '/hey') {
+    mytext ="well hello there..."
+    } else {
+      mytext = 'I dont know you!';
+    }
+
+    myresponse.writeHead(200, {"Content-Type": "text/plain"} );
+    myresponse.end(mytext +'\n');
+  }
+);
+myserver.listen(8080, "0.0.0.0");
+
+console.log('sever has started')
+ 
